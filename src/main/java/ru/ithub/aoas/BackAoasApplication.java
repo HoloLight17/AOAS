efd63c59eb6b53dc5d431945a14fd419ec6ed550
spechurkin <pechurkins@gmail.com>
2022-05-17 11:11:36 +0300

    minor

--- src/main/java/ru/ithub/aoas/BackAoasApplication.java
+++ src/main/java/ru/ithub/aoas/BackAoasApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AoasApplication {
+public class BackAoasApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AoasApplication.class, args);
+        SpringApplication.run(BackAoasApplication.class, args);
     }
 
 }
